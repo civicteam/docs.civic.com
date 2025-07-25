e95b6d5d16b27c6772e791b44269b6dd94ce178f
tyronemichael <tyronemichael@gmail.com>
2025-07-25 10:44:27 -0500

    Add Kapa AI widget
    - Update logo to use the new logo
    - Add Kapa AI widget to the docs

---
+++ kapa-widget.js
@@ -0,0 +1,8 @@
+const script = document.createElement("script");
+script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+script.async = true;
+script.setAttribute("data-website-id", "6b20664c-1b0b-46ef-9801-3c7b0fac7b2d");
+script.setAttribute("data-project-name", "Civic Docs");
+script.setAttribute("data-project-color", "#CB86AB");
+script.setAttribute("data-project-logo", "https://docs.civic.com/images/logo.png");
+document.head.appendChild(script); 
