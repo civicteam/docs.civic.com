e37dd20f5255094ecbaafdaaa9d6265921c43165
tyronemichael <tyronemichael@gmail.com>
2025-07-25 10:47:11 -0500

    Update Kapa AI widget logo

--- kapa-widget.js
+++ kapa-widget.js
@@ -4,5 +4,5 @@ script.async = true;
 script.setAttribute("data-website-id", "6b20664c-1b0b-46ef-9801-3c7b0fac7b2d");
 script.setAttribute("data-project-name", "Civic Docs");
 script.setAttribute("data-project-color", "#CB86AB");
-script.setAttribute("data-project-logo", "https://docs.civic.com/images/logo.png");
+script.setAttribute("data-project-logo", "https://cdn.prod.website-files.com/6721152f5cf7d1402980ed13/6724ff46d44044c6b1599154_civic-logo-white.svg");
 document.head.appendChild(script); 
