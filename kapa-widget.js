a913e026bf8a2f6a18d36a66f1eb31de22579e01
tyronemichael <tyronemichael@gmail.com>
2025-07-29 11:24:24 -0500

    Update kapa-widget.js

--- kapa-widget.js
+++ kapa-widget.js
@@ -5,4 +5,5 @@ script.setAttribute("data-website-id", "6b20664c-1b0b-46ef-9801-3c7b0fac7b2d");
 script.setAttribute("data-project-name", "Civic Docs");
 script.setAttribute("data-project-color", "#7C75FF");
 script.setAttribute("data-project-logo", "https://civic-public-logo-dev.s3.us-east-1.amazonaws.com/civic-ai-white.png");
+script.setAttribute("data-modal-image", "https://civic-public-logo-dev.s3.us-east-1.amazonaws.com/civic-ai.png");
 document.head.appendChild(script); 
