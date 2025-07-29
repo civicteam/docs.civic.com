12d17df17c2210f9b4fdfe205f23f31768bdc2ac
tyronemichael <tyronemichael@gmail.com>
2025-07-29 11:11:32 -0500

    Make logo white for kapa-widget.js

--- kapa-widget.js
+++ kapa-widget.js
@@ -4,5 +4,5 @@ script.async = true;
 script.setAttribute("data-website-id", "6b20664c-1b0b-46ef-9801-3c7b0fac7b2d");
 script.setAttribute("data-project-name", "Civic Docs");
 script.setAttribute("data-project-color", "#7C75FF");
-script.setAttribute("data-project-logo", "https://civic-public-logo-dev.s3.us-east-1.amazonaws.com/civic-ai.png");
+script.setAttribute("data-project-logo", "https://civic-public-logo-dev.s3.us-east-1.amazonaws.com/civic-ai-white.png");
 document.head.appendChild(script); 
